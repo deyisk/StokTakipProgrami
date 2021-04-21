8e57031df537dbc560a4a60002ec36f64ff10972
deyisk <deyiskarakas@hotmail.com>
2021-04-21 17:06:54 +0300

    ikinci gonderim1

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,10 +6,10 @@ import Login from "../main/login/Login";
 function Navbar() {
   return (
     <nav className="navbar">
-      {/* <div className="button"> */}
-      <i className="fa fa-user-link"></i>
-      {/* <Link to="/login">Login</Link> */}
-      {/* </div> */}
+      <div className="button">
+        <i className="fa fa-user-link"></i>
+        <Link to="/login">Login</Link>
+      </div>
       <div className=""></div>
       <div className="navbar__right">
         <a href="#"></a>
